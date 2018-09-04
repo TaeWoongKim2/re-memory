baa798de0673a69f7edf3ce4715e2db938e81233
khj <gimijuny@gmail.com>
2018-09-04 14:47:42 +0900

    회원가입 및 로그인 페이지 Pug로 변환

--- projects/myapp/routes/index.js
+++ projects/myapp/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET sing-up page. */
+router.get('/sign-up', function(req, res, next) {
+  res.render('sign-up', { title: 'sign-up' });
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'login' });
+});
+
 module.exports = router;
