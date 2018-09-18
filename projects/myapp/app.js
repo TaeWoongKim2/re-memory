b40aeca2b73e52d24cd5eef725cfd9bc0197c392
Woong <ktaewoong46@gmail.com>
2018-09-18 13:58:03 +0900

    app.js 부활
    app.js 다시 넣음

---
+++ projects/myapp/app.js
@@ -0,0 +1,84 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var session = require('express-session');
+var redis = require('redis');
+var RedisStore = require('connect-redis')(session);
+var config = require('./config/config.json');
+var client = redis.createClient();
+
+const passport = require('passport');
+const passportConfig = require('./passport');
+const flash = require('connect-flash');
+require('dotenv').config();
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const { sequelize } = require('./models');
+
+var app = express();
+sequelize.sync();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(express.static(path.join(__dirname, 'public')));
+
+passportConfig(passport);
+
+app.use(session({
+  store: new RedisStore({ host: config.redis.host, port: config.redis.port, client: client }),
+  key: config.session.key,
+  secret: config.session.secret,
+  cookie: {
+    maxAge: 1000 * 60 * 60
+  },
+  saveUninitialized: false,
+  resave: false
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+
+/**
+ * http://usejsdoc.org/
+ */
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
