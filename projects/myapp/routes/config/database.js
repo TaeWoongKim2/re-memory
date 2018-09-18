05c9503e96914ee99305008c19605d3073719eb8
Woong <ktaewoong46@gmail.com>
2018-09-18 13:51:04 +0900

    config confilct 해결

--- projects/myapp/routes/config/database.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-    host : "rootmark.cum15mhnk9mh.ap-northeast-2.rds.amazonaws.com",
-    user : "root",
-    password : "roqkfwk1004",
-    database : "memorize",
-    port : "3306"
-}
